8e2efeaedee92a0be5138d8336b5b2c57c0c7c0b
Anil <Anil.appari007@gmail.com>
2022-05-31 18:39:32 +0530

    added date

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <p>
+//            <code>React Deployment using Bamboo</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// -------------------------------------
+// -------------------------------------
+
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Edit <code>React Deployment using Bamboo</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+class App extends React.Component {
+  state={
+    curDT : new Date().toLocaleString(),
+  }
+  render(){
+    return (
+      <div className="App">
+        <p>React Deployment using Bamboo</p>
+        <p>Reload to check the time change</p>
+        <p>Current Date And Time : {this.state.curDT}</p>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
