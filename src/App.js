02b7cf93c512d48f335a29472845904cfcbfa65c
Anil <Anil.appari007@gmail.com>
2022-06-01 09:39:50 +0530

    commented logo

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@
 // -------------------------------------
 
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
  
 class App extends React.Component {
